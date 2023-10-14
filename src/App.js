bc516e6b23e5f1726058357c3fd0a002846405cf
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-10-14 14:22:14 +0530

    React chat engine

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChatEngine
+        height='100vh'
+        projectID='98e29863-578e-4066-ae65-cf9742013c9c'
+        userName='dulanjali'
+        userSecret='123456789' />
     </div>
   );
 }
