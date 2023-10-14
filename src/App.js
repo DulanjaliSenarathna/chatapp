ad1a705077a0694b969e00b8f32ec743ce62935b
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-10-14 15:30:45 +0530

    Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
-import { ChatEngine } from 'react-chat-engine';
+import { ChatEngine} from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed';
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
         height='100vh'
         projectID='98e29863-578e-4066-ae65-cf9742013c9c'
         userName='dulanjali'
-        userSecret='123456789' />
+        userSecret='123456789'
+        renderChatFeed= {(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+        onNewMessage={()=> new Audio('https://chat-engine-assets.s3. amazonaws.com/click.mp3')} />
     </div>
   );
 }
